648fe6ace10aff3eaf78a90ed21a0bfa5b510dc3
Henkka <henkkawar@gmail.com>
2019-04-17 23:01:30 +0500

    task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -4,36 +4,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
 
 public class Task01Main {
+    private static final String statisticInfo = "ffprobe -v error -of flat -show_format";
+    private static final String tagTitle = "format.tags.title=";
+
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        return extractSomeFeature(file, "ffprobe -v error -of flat -show_format", "format.tags.title=");
+        return extractSomeFeature(file, statisticInfo, tagTitle);
     }
 
     public static String extractSomeFeature(File file, String query, String feature) throws IOException, InterruptedException {
-        if (file == null || query == null || feature == null){
-            throw new NullPointerException();
-        }
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command((query + " " + file.getName()).split(" "))
+        if (file == null || query == null || feature == null) throw new NullPointerException();
+        Process process = new ProcessBuilder()
+                .command((query + " " + file.getName()).split(" "))
                 .directory(new File(file.getParent()))
-                .redirectInput(ProcessBuilder.Redirect.from(new File("/dev/null")))
-                .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                .redirectError(ProcessBuilder.Redirect.INHERIT); ///whatahul
-        Process process = processBuilder.start();String rezult;
-        try(BufferedReader reader = new BufferedReader(
+                .start();
+        String result;
+        try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(process.getInputStream())
-        )){
-            rezult = reader.lines().
+        )) {
+            result = reader.lines().
                     filter(x -> x.startsWith(feature))
-                    .findAny().orElse("");
+                    .findAny().orElse(null);
         }
-        int exitVakue = process.waitFor();
-        return rezult.substring(feature.length() + 1, rezult.length() - 1);
+        int successValue = process.waitFor();
+        if (successValue != 0)
+            return "Process terminated with value: " + successValue;
+        return result == null ? "not found info" : result.substring(feature.length() + 1, result.length() - 1);
     }
 }
