487bae0114659fe9a595d7b98e15c82cd517c24f
Алексей Ступников <stuq11@gmail.com>
2019-04-18 20:26:35 +0500

    Задача 01 решена

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,19 +2,35 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String result = "";
+
+        ProcessBuilder processBulder = new ProcessBuilder();
+        processBulder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        Process process = processBulder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        try {
+            String[] lines = reader.lines().toArray(String[]::new);
+            for (String line : lines)
+                if (line.indexOf("format.tags.title") == 0) {
+                    result = line.substring(19, line.length() - 1);
+                    break;
+                }
+        } catch (Exception e) {
+            throw new RuntimeException("Имя файла не найдено");
+        } finally {
+            reader.close();
+        }
+
+        return result;
     }
 }
