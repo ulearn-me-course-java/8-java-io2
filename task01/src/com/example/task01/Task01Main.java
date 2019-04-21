42eabcd0fe6bac94700f093ab0dff58fe19d16f1
AllaOsipyan <allosipyan@mail.ru>
2019-04-21 20:01:25 +0500

    Осипян Пайцар. Задание01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,11 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -9,12 +13,29 @@ public class Task01Main {
         // например вот так:
 
         /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
+        System.out.println(extractSoundName(new File("task01/src/main/resources/3724.mp3")));
         */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe.exe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Process process = processBuilder.start();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String[] lines=reader.lines().toArray(String[]::new);
+            for (String el:lines
+                 ) {
+                int index= el.indexOf("=");
+                if((el.substring(0,index)).equals("format.tags.title")) {
+                     return el.substring(index+2, el.lastIndexOf("\""));
+                }
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return "";
+
     }
 }
