f50eb473d53ae69d6001252d8716fdf18b1cb11b
Max <uue33@mail.ru>
2019-04-21 17:59:00 +0500

    Иконникова Анастасия. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,8 +1,15 @@
 package com.example.task02;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.*;
+import java.util.LinkedList;
 import java.util.List;
+import java.nio.file.Files;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -16,8 +23,10 @@ public class Task02Main {
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> paths = new LinkedList<>();
+        Files.walk(rootDir)//поток, который проходится по дереву с начальной позиции rootDir
+                .filter(Files::isRegularFile)// является ли файл обычным файлом
+                .forEach(paths::add);//естественный способ обхода файлов
+        return paths;
     }
 }
