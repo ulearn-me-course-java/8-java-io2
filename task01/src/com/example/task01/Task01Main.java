9cda7e75744cfdb8215510d8ec3b2e05b2d40742
Pavel <pavel.zaborin@inbox.ru>
2019-04-21 23:05:16 +0500

    Заборин Павел. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,29 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+        System.out.println(extractSoundName(
+                new File("/Users/pavelzaborin/IdeaProjects/8-java-io2/task01/src/main/resources/Savage-OnlyYou.mp3")));
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String[] strings = reader.lines().toArray(String[]::new);
+            for (String string : strings) {
+                if (string.indexOf("format.tags.title") == 0) {
+                    return string.substring(19, string.length() - 1);
+                }
+            }
+        }
+        throw new RuntimeException();
     }
 }
