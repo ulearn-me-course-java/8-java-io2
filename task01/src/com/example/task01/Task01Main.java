184742c5dffec5684626bbff632644eee7ba6cfd
Platonov2 <jek1999060@mail.ru>
2019-04-23 16:57:47 +0500

    Платонов Евгений. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,39 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("C:\\Users\\jek19\\Desktop\\ffmpeg-20190423-a087764-win64-static\\bin\\ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Process process = processBuilder.start();
+        String string = null;
+
+        try (Scanner scanner = new Scanner(new InputStreamReader(process.getInputStream()))) {
+            while (scanner.hasNext()){
+                string = scanner.nextLine();
+
+                if (string.contains("format.tags.title")){
+                    string = string.substring(string.indexOf("\"") + 1, string.lastIndexOf("\""));
+                    break;
+                }
+            }
+        }
+        return string;
     }
 }
