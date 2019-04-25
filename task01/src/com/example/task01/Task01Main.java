9df493401269f6b3a5d7ca36d5179f09cee3bd95
Vitaliy Smichkovskiy <32594950+ProgressiveArt@users.noreply.github.com>
2019-04-25 20:35:19 +0300

    Задание 01 Смычковский В.
    # Задание 01 Запуск процесса
    
    Напишите код, который позвляет извлечь название звукового трека из mp3-файла.
    
    Для этого вам понадобится программа ffmpeg, установите ее отсюда - http://ffmpeg.org/download.html
    
    Пример команды для получения нужной информации:
    ```
    ffprobe -v error -of flat -show_format file.mp3
    ```
    
    Формат ответа на команду:
    ```
    ...
    format.tags.title="Canon trias harmonica a 8"
    format.tags.album="Canons"
    format.tags.artist="Johann Sebastian Bach"
    format.tags.genre="Classical"
    ```
    
    - Запустите эту команду как подпроцесс из java
    - Найдите в выводе свойство "format.tags.title"
    - Верните из метода extractSoundName значение этого свойства

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,19 +2,36 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+
+        String res = null;
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("C:\\FFmpeg\\bin\\ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        Process process = processBuilder.start();
+
+        try (Scanner scan = new Scanner(new InputStreamReader(process.getInputStream()))) {
+            while (scan.hasNext()) {
+                res = scan.nextLine();
+
+                if (res.contains("format.tags.title")) {
+                    res = res.substring(19, res.length() - 1);
+                    break;
+                }
+            }
+        }
+        return res == null ? "not found info" : res;
     }
 }
