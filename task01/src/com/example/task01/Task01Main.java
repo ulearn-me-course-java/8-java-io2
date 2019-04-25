ddd539f343a1a4627aeabdacca3a3bae82e598d4
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-04-25 23:14:34 +0500

    Update Task01Main.java

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,8 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -14,7 +16,23 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String res = null;
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("C:\\FFmpeg\\bin\\ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        Process process = processBuilder.start();
+
+        try (Scanner scan = new Scanner(new InputStreamReader(process.getInputStream()))) {
+            while (scan.hasNext()) {
+                res = scan.nextLine();
+
+                if (res.contains("format.tags.title")) {
+                    res = res.substring(19, res.length() - 1);
+                    break;
+                }
+            }
+        }
+        return res == null ? "not found info" : res;
     }
 }
