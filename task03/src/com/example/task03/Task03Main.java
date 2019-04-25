b601461c621d0d440849cea53fd5721c64451d12
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-04-25 23:15:25 +0500

    Update Task03Main.java

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,6 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -15,7 +14,12 @@ public class Task03Main {
     }
 
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
-        // your implementation here
-        return null;
+        SampleData data = null;
+
+        try(ObjectInputStream objectStream = new ObjectInputStream(inputStream)){
+            data = (SampleData)objectStream.readObject();
+        }
+
+        return data;
     }
 }
