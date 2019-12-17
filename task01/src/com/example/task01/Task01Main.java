8f33cf6d0870dfe70e78bd92190eb74e7d239fb9
vkalschikov@gmail.com <Rfkmobrjd11>
2019-12-17 21:05:39 +0500

    Решение задачи 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,6 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -14,7 +13,18 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String name = null;
+        StringBuilder call = new StringBuilder("ffprobe -v error -of flat -show_format ");
+        call.append(file.getAbsolutePath());
+        Process proc = Runtime.getRuntime().exec(call.toString());
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            String splitted[] = s.split("=");
+            if (splitted[0].equals("format.tags.title") && splitted.length > 1) {
+                name = splitted[1];
+            }
+        }
+        return name;
     }
 }
