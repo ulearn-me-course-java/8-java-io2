e94f676c3947047252457126c4d36130ebddd3a1
genwarhunter <kostyarik98@mail.ru>
2019-12-17 22:28:12 +0500

    commit

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,6 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -14,7 +13,16 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String[] input = reader.lines().toArray(String[]::new);
+            for (String str : input)
+                if (str.contains("format.tags.title"))
+                    return str.substring(str.lastIndexOf('=') + 2, str.length() - 1);
+        }
+        throw new RuntimeException();
     }
 }
