dd583c951608d5361feae441bdb5dd7a8698149a
Валерия Самсонова <samsonova147@hotmail.com>
2019-12-22 17:04:03 +0500

    [Самсонова] Решение задачи 8.1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,7 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.*;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,6 +16,16 @@ public class Task01Main {
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
         // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String[] input = reader.lines().toArray(String[]::new);
+            for (String str : input)
+                if (str.contains("format.tags.title"))
+                    return str.substring(str.lastIndexOf('=') + 2, str.length() - 1);
+        }
+        throw new InterruptedException();
     }
 }
