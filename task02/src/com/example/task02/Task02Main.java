a3d221cdb5f91ff2aec1431fb9e0dd336644bdbc
Валерия Самсонова <samsonova147@hotmail.com>
2019-12-22 17:19:27 +0500

    [Самсонова] Решение задачи 8.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,6 +3,9 @@ package com.example.task02;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.LinkedList;
+import java.nio.file.Files;
+
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -18,6 +21,8 @@ public class Task02Main {
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
         // your implementation here
 
-        return null;
+        List<Path> paths = new LinkedList<>();
+        Files.walk(rootDir).filter(Files::isRegularFile).forEach(paths::add);
+        return paths;
     }
 }
