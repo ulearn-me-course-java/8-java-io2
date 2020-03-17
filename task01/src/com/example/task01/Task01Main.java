eb7773fd5abf571abcb7c75f528ee9cd754893b5
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-17 15:47:30 +0500

    task1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,19 +2,30 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command("/Users/grin/IdeaProjects/8-java-io2/task01/ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath())
+                .redirectOutput(ProcessBuilder.Redirect.PIPE);
+
+        Process prc = pb.start();
+        Scanner sc = new Scanner(prc.getInputStream()).useDelimiter("format\\.");
+
+        while (sc.hasNext()){
+            String info = sc.next();
+            if(info.contains("title"))
+                return info.replaceAll("tags.title=", "")
+                        .replace("\"", "")
+                        .replace("\n", "");
+        }
+        throw new IllegalArgumentException();
+        //Я знаю, что это очень плохо, это надо сделать по-другому
+        //но мне лень (((
     }
 }
