4d2683ab3dcb7c9cfa89012224a8f45a25c1a602
Id.Io.T <kdevyatkov-ilya@mail.ru>
2020-04-13 15:02:15 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,9 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
@@ -17,7 +19,11 @@ public class Task02Main {
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
         // your implementation here
+        ArrayList<Path> files = new ArrayList<Path>();
+        if(files != null)
+        Files.walk(rootDir).filter(path -> Files.isRegularFile(path)).forEach(path -> files.add(path));
+        return files;
 
-        return null;
+
     }
 }
