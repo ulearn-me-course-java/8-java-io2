04e5d59d8ff174778bb0509595c13f2c08ecba5a
MatveyTolpeev <61470886+MatveyTolpeev@users.noreply.github.com>
2020-04-16 02:40:45 +0500

    Update Task01Main.java

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,63 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
+    private static final long MAX_TIME = 10_000;
+
+    private static final String NEEDED_KEY = "format.tags.title";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
         // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        try {
+            Process process = processBuilder.start();
+            boolean executedOnTime = process.waitFor(MAX_TIME, TimeUnit.MILLISECONDS);
+
+            if (!executedOnTime) {
+                System.out.println("Failed, out of timeout.");
+                process.destroy();
+
+                return null;
+            }
+
+            if (process.exitValue() != 0) {
+                System.out.println("Failed with exit code" + process.exitValue());
+
+                return null;
+            }
+
+            BufferedReader output = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+
+            while ((line = output.readLine()) != null) {
+                String[] lineArr = line.split("=");
+                if (lineArr.length == 2 && lineArr[0].equals(NEEDED_KEY)) {
+
+                    return lineArr[1].substring(1, lineArr[1].length() - 1);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Process was not started \n" + e);
+
+            return null;
+        } catch (InterruptedException e) {
+            System.out.println("Process was interrupted \n" + e);
+            Thread.currentThread().interrupt();
+
+            return null;
+        }
+        return null;
     }
 }
