86f66b3e8f74bca1a79703d637656cf4dca7c482
Jin <jin1310@icloud.com>
2020-04-16 20:12:39 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,8 +1,12 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -16,8 +20,11 @@ public class Task02Main {
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        ArrayList<Path> filesBypass = new ArrayList<>();
 
-        return null;
+        Files.walk(rootDir)
+                .filter(path -> Files.isRegularFile(path))
+                .forEach(path -> filesBypass.add(path));
+        return filesBypass;
     }
 }
