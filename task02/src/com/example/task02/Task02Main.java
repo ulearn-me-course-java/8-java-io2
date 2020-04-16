a35609d968ccb8c4963d46b5ed5ecbb80f5a04e9
Ekaterina <slalam.ww@gmail.com>
2020-04-16 12:16:36 +0500

    Абрамкова, 8.02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,29 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+        //Реализуйте рекурсивный обход заданного каталога, используя java.nio
+        //
+        //Результатом работы метода listFiles должен быть список все файлов в каталоге rootDir.
 
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
 
-        return null;
+        ArrayList<Path> filesBypass = new ArrayList<>();
+
+            Files.walk(rootDir)
+                    .filter(path -> Files.isRegularFile(path))
+                    .forEach(path -> filesBypass.add(path));
+        return filesBypass;
     }
 }
