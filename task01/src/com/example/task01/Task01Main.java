191e4619a1745bd397c2f9dc1019be390bb8b348
Darina <37955869+dagenator@users.noreply.github.com>
2021-04-05 13:27:19 +0500

    Габидуллина Дарина task 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,6 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -14,7 +13,22 @@ public class Task01Main {
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ls","-l")
+                .directory(new File("C:\\Users\\gabid\\OneDrive\\Рабочий стол\\ffmpeg-2021-04-04-git-b1b7cc698b-full_build\\bin"))
+                .command("cmd.exe", "/c", "ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath())
+                .redirectOutput(ProcessBuilder.Redirect.PIPE); //контролируем поток вывода
+
+        Process process = processBuilder.start();
+        BufferedReader  reader = new BufferedReader( new InputStreamReader(process.getInputStream()));
+        String result = reader.readLine();
+        while(result != null){
+            if(result.contains("title")){
+                return result.substring(result.indexOf("\"")+1, result.lastIndexOf("\""));
+            }
+            result = reader.readLine();
+        }
         return "sound name";
     }
 }
