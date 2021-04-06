a75c7390bffdfc4f62aafa858be68b3daf663193
1 <hstllbg@gmail.com>
2021-04-06 13:08:06 +0500

    Петров Игорь. Задание 8.1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,29 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+
+        String soundName;
+        ProcessBuilder processBuilder = new ProcessBuilder()
+                .command("/Users/igorpetrov/Downloads/ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        Process process = processBuilder.start();
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            soundName = reader.lines().filter(a -> a.contains("format.tags.title")).findFirst().get().split("=")[1];
+        }
+
+        return soundName.length() > 0 ? soundName.substring(1, soundName.length() - 1) : null;
     }
 }
