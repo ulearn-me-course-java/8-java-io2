73ff45aa8722d8b6e3459abebd7cb02c3518d437
DarkLight <max.gubar@outlook.com>
2021-04-14 20:05:26 +0500

    Губарь Максим task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,10 +1,18 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
+    public List<Path> result = new ArrayList<>();
+
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
@@ -16,8 +24,8 @@ public class Task02Main {
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> result = new ArrayList<>();
+        Files.walk(Paths.get(rootDir.toString())).filter(Files::isRegularFile).forEach(result::add);
+        return result;
     }
 }
