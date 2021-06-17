0de67826e6c8365be3b3ce495be13d21c2dc2f86
rewqty <boss.dima0000@gmail.com>
2021-06-17 23:55:42 +0500

    Челноков Дмитрий task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,35 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String title;
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        Process process = processBuilder.start();
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+            title = reader.lines()
+                    .filter(line -> line.contains("title"))
+                    .findFirst()
+                    .get();
+        }
+
+        int start = title.indexOf('\"') + 1;
+        int end = title.indexOf('\"', start);
+        return title.substring(start, end);
     }
 }
