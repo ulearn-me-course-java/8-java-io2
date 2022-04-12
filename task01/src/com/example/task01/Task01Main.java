9b893a6d27162792bde58f9a11303f53fea5e16d
Saveliy <savelussava@mail.ru>
2022-04-12 20:47:00 +0500

    Task1. Кудрявцев Савелий

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,30 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+       System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder process = new ProcessBuilder("cmd.exe", "/c", "ffprobe -v error -of flat -show_format " + file.getAbsolutePath());
+        process.directory(new File("C:\\Users\\Lenovo\\IdeaProjects\\8-java-io2\\task01\\src\\com\\example\\task01"))
+                .redirectOutput(ProcessBuilder.Redirect.PIPE);
+        try(BufferedReader buffer = new BufferedReader(new InputStreamReader(process.start().getInputStream()))) {
+            String txt = buffer.readLine();
+            while (txt != null){
+                if (txt.contains("format.tags.title"))
+                    return txt.split("\"")[1];
+                txt = buffer.readLine();
+            }
+        }
+        return null;
     }
+
 }
