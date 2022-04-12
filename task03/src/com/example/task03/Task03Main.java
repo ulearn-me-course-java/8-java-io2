805d42fabf40f888317b92c68e0305ace272167c
FremyXS <antonovch13@mail.ru>
2022-04-12 23:23:51 +0500

    Задание 8.3 Овчинников Антон ПРИ-202

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,9 @@
 package com.example.task03;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -12,10 +14,13 @@ public class Task03Main {
         System.out.println(deserialize(new FileInputStream("task03/src/main/resources/example1.bin")));
         */
 
+        System.out.println(deserialize(new FileInputStream("task03/src/main/resources/example1.bin")));
+
     }
 
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
-        // your implementation here
-        return null;
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)){
+            return (SampleData) objectInputStream.readObject();
+        }
     }
 }
