2433a87ac2211ac7c88f8810c608c4f6a7a276ac
Degener8 <raptoroidinbox@gmail.com>
2022-04-13 02:02:28 +0500

    Гриценюк task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,19 +2,33 @@ package com.example.task01;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+
+        ProcessBuilder process = new ProcessBuilder("cmd.exe","/c","ffprobe -v error -of flat -show_format " + file.getAbsolutePath());
+        process.directory(new File("D:\\JavaTasks\\8-java-io2\\ffmpeg\\bin"));
+
+        try(BufferedReader buffer = (new BufferedReader(new InputStreamReader(process.start().getInputStream()))))
+        {
+            String input = buffer.readLine();
+            while(input != null)
+            {
+                if(input.contains("format.tags.title"))
+                    return input.split("\"")[1];
+
+                input = buffer.readLine();
+            }
+        }
+
+        return "error";
     }
 }
