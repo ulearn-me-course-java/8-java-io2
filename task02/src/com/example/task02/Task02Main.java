d381d5a900402fb3fc15fe882d10b4af3e70599a
gorosheek <yulyagroshenko@mail.ru>
2022-04-20 01:13:45 +0500

    Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,11 +1,16 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Task02Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+public class Task02Main
+{
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
@@ -15,9 +20,10 @@ public class Task02Main {
 
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException
+    {
+        return Files.walk(rootDir)
+                .filter(path -> path.toFile().isFile())
+                .collect(Collectors.toList());
     }
 }
