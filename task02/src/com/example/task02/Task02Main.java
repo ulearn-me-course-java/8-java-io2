e70009a5e9dddea30ff8c8c34378ca90c45aef08
Мелёхина Виктория <sweet_fox7940107@mail.ru>
2022-12-05 10:09:06 +0500

    all

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,10 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
@@ -9,15 +12,33 @@ public class Task02Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+
 
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException,NullPointerException {
 
-        return null;
+        List<Path> files = new ArrayList<>();
+        try {
+            for (File file : rootDir.toFile().listFiles()) {
+                try {
+                    if (file.isDirectory()) {
+                        files.addAll(listFiles(file.toPath()));
+                    } else {
+                        files.add(file.toPath());
+                    }
+                } catch (IOException e) {
+                    throw new IOException();
+                } catch (InterruptedException e) {
+                    throw new InterruptedException();
+                }
+            }
+        } catch ( NullPointerException e) {
+            throw new NullPointerException();
+        }
+
+        return files;
     }
 }
