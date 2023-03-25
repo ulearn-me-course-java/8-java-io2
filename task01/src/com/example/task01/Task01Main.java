ad6e3bf8e9c7ab63d8e6eade0bef7eb7da22df8a
ilya <27romashka05@gmail.com>
2023-03-25 17:15:42 +0500

    Task01 Квашнин Илья ПрИ-201

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,40 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+        System.out.println(extractSoundName(new File("task01/src/main/resources/3726.mp3")));
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/bin/bash", "-c", "ffprobe -v error -of flat -show_format " + "\""+ file.getAbsolutePath() + "\"")
+                .directory(new File("/opt/homebrew/Cellar/ffmpeg/5.1.2_6/bin"))
+                .redirectInput(ProcessBuilder.Redirect.from(new File("/dev/null")))
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .redirectError(ProcessBuilder.Redirect.INHERIT);
+
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader( //try гарантирует завершение и вызов метода .close()
+                new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if(line.contains("title")) {
+                    return line.split("=")[1].replace("\"", "");
+                }
+            }
+        }
+
+        int exitValue = process.waitFor();
+        if (exitValue != 0) {
+            System.err.println("Subprocess terminated abnormally");
+        }
+
+        return null;
     }
 }
