0fb06bddf24122984785dc2f7cfbef57ca3745eb
Michael Gallinago <michael.gallinago@gmail.com>
2023-09-25 15:13:03 +0500

    Lab8

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,35 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+        Tests tests = new Tests();
+        tests.testRoot();
+        tests.testA();
+        tests.testE();
 
-        /*
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> result = new ArrayList<>();
+        if (rootDir.toFile().isDirectory()) {
+            for (File item : Objects.requireNonNull(rootDir.toFile().listFiles())) {
+                if (item.isDirectory()) {
+                    result.addAll(listFiles(item.toPath()));
+                } else {
+                    result.add(item.toPath());
+                }
+            }
+        }
+        return result;
     }
 }
