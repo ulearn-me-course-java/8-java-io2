810cbc6d602fafb8c85dfa5ad847a4e4f988af1d
pe4enka <girfanov_kostia@mail.ru>
2023-11-18 19:40:54 +0500

    task 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,32 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command("C:\\Users\\pepenka\\Desktop\\ffmpeg-6.1-essentials_build\\bin\\ffprobe.exe",
+                "-v", "error", "-of", "flat", "-show_format", file.getCanonicalPath());
+        Process process = builder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if(line.split("=")[0].equals("format.tags.title")){
+                return line.split("=")[1].replace("\"", "");
+            }
+        }
+
+        return "";
     }
 }
+
