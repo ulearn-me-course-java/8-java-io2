6d3cadd8f4d7f6ac6dc8d5fd78f938541e08eea5
pe4enka <girfanov_kostia@mail.ru>
2023-11-18 20:41:26 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,9 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task02Main {
@@ -9,15 +11,25 @@ public class Task02Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
 
-        return null;
+        List<Path> files = new ArrayList<Path>();
+        File[] dir = rootDir.toFile().listFiles();
+
+        for (File file : dir) {
+            if(file.isDirectory()){
+                files.addAll(listFiles((file.toPath())));
+            }
+            else{
+                files.add(file.toPath());
+            }
+        }
+        return files;
     }
 }
