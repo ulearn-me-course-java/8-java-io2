b294133e902acf2017d8792ded9c10ec75fd8d5b
mibelmay <zemcovannastasia11@gmail.com>
2023-11-18 17:22:13 +0500

    module 8

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,36 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.nio.file.DirectoryStream;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
-
+        System.out.println(listFiles(Paths.get("src/main/resources/")));
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> files = new ArrayList<>();
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(rootDir)) {
+            for (Path path : stream) {
+                if(path.toFile().isDirectory()) {
+                    files.addAll(listFiles(path));
+                } else {
+                    files.add(path);
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return files;
     }
 }
