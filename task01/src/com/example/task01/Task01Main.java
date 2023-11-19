696fa7e148ad6114b3ee744b6d856b63b5ea2eee
KatyaPark11 <127592285+KatyaPark11@users.noreply.github.com>
2023-11-19 19:04:42 +0500

    Федотова Екатерина. Задание 01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,38 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 public class Task01Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\User\\Downloads\\Асия-Останься.mp3");
+        System.out.println(extractSoundName(file));
     }
 
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+    public static String extractSoundName(File file) {
+        List<String> command = Arrays.asList("ffprobe", "-v", "error",
+                "-of", "flat=s=_", "-show_format", file.getAbsolutePath());
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.redirectErrorStream(true);
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader processOutput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = processOutput.readLine()) != null) {
+                if (line.contains("title")) {
+                    return line.substring(line.indexOf('"')).replace("\"", "");
+                }
+            }
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Ошибка выполнения команды: " + e.getMessage());
+        }
+        return null;
     }
 }
